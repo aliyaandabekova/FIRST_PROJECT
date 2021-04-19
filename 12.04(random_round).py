0e13b56ebdc098797b30372d2846b3f0b947fa35
aliyaandabekova <andabekovaaliyaa@gmail.com>
2021-04-19 17:32:53 +0300

    random_round

---
+++ 12.04(random_round).py
@@ -0,0 +1,88 @@
+# print(type(x))
+# x = 5.0
+# print(isinstance(x,int))
+
+# prise = "500"
+# if isinstance(prise,int):
+#     print(prise,"OK")
+# else:
+#     price = int(prise)
+#     print(prise,"NOT OK")
+
+# import random
+# secret_number = random.randint(1,10)
+# print(secret_number)
+# guess_number = int(input("Число от 1 до 10: "))
+# if secret_number == guess_number:
+#     print("chocolate")
+# else:
+#     print("=(")
+
+ # import random
+ # secret_number = random.random()
+ # print(secret_number)
+
+# import random
+# secret_number = random.sample([1,2,3,4,5,6,7,8,9,0],4)
+# print(secret_number)
+
+# print("Задачи")
+
+# print(round(5.45555566311477634486212,4))
+# print(round(5.245))
+
+# money = float(input())
+# percentage = 10
+# years = int(input())
+# money_percentage = money + (money * 0.1)
+# som = int(money_percentage / 1)
+# print("Som = ",som)
+# result = money_percentage % 1
+# print(result)
+# result = round(result,2)
+# tyiyn = result * 100
+# tiyin = int(tyiyn)
+# print("Tiyin = ",tiyin )
+
+# price = 100.30
+# quantity = 3
+# result = price * quantity
+# print(result)
+# som = result // 1
+# print(som)
+# tiyin = result % 1
+# result = round(tiyin,2)
+# result = result * 100
+# print("Som = ",som)
+# print("Tiyin = ",result)
+
+
+# print("ДОМАШНЕЕ ЗАДАНИЕ")
+#
+# import random
+# distance = int(input())
+# workload = random.randint(50,150)
+# print("Workload = ",workload)
+# start_sum = 0
+# som_per_km = 0
+# force = 0
+# if workload < 50:
+#     start_sum = 70
+#     som_per_km = 15
+# elif workload < 80 and workload > 50:
+#     start_sum = 65
+#     som_per_km = 13
+# else:
+#     start_sum = 39
+#     som_per_km = 10
+# if distance > 30:
+#     force = 1000
+# elif distance > 15 and distance < 30:
+#     force = 500
+# else:
+#     force = 0
+# total = start_sum + (som_per_km * distance) + force
+# print("To pay = ",total)
+
+
+
