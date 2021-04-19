a16cfec83c00be12e4ac108178cf1e28bf8a19d8
aliyaandabekova <andabekovaaliyaa@gmail.com>
2021-04-19 16:15:48 +0300

    09.04(tochno if)

---
+++ 09.04(if_else_elif).py
@@ -0,0 +1,225 @@
+# x = int(input())
+# if x <= 10:
+#     print("YES")
+# else:
+#     print("NO")
+
+# x = int(input())
+# y = int(input())
+# if x > y:
+#     print(x)
+# else:
+#     print(y)
+
+# x = int(input())
+# if x % 2 == 0:
+#     print("Even number")
+# else:
+#     print("Not even")
+
+# a = int(input())
+# b = int(input())
+# if a > b:
+#     print (b)
+# else:
+#     print (a)
+
+# print(len("ogogo"))
+# print(len("hello "))
+
+# password = input('Enter password: ')
+# check_password = input('Confirm password: ')
+# if password == check_password:
+#     print("Succesful")
+# else:
+#     print("Password too short!")
+
+
+# password = input('Enter password: ')
+# check_password = input('Confirm password: ')
+# if password == check_password and len("password") >=8:
+#     print("Succesful")
+# else:
+#     print("Password too short!")
+
+
+# password = input('Enter password: ')
+# check_password = input('Confirm password: ')
+# if password == check_password and len(password) >=8:
+#     print("Succesful")
+# else:
+#     if len(password) >= 8:
+#         print("Password dont match!")
+#     else:
+#         print("Password too short!")
+
+
+# x = int(input())
+# y = int(input())
+#
+# if x > 0:
+#     if y > 0:
+# print("недоделано")
+
+
+
+
+# number1 = int(input())
+# if number1 > 1:
+#     print(1)
+# elif number1 == 0:
+#     print(0)
+# else:
+#     print(-1)
+
+
+
+
+# A = int(input())
+# B = int(input())
+# C = int(input())
+# if A >= B and A >= C:
+#     print(A)
+# elif B >= A and B >= C:
+#     print(B)
+# else:
+#     print(C)
+
+# steps = int(input())
+# print(steps % 6)
+
+
+
+# x = float(input())
+# print(x - int(x))
+
+
+
+# string1 = "ngut jiubtjn nbikb nubt Navalny nuth nuhbutji nbtuii"
+# ban_word = "Navalny"
+# if ban_word in string1:
+#     print("NOT OK")
+# else:
+#     print("OK!")
+
+
+
+# string1 = "ngut jiubtjn nbikb nubt Navaln nuth nuhbutji nbtuii"
+# ban_word = "Navalny"
+# if ban_word in string1:
+#  del string1
+# else:
+#  print("OK!")
+
+# V_engine1 = 20
+# V_fuel = 100
+# k = 100
+# distance = int(input())
+# result = (100 / 20 ) * k
+# if distance <= result:
+#     print("OK")
+# else:
+#     print("NOT OK!!!")
+#     result2 = distance - result
+#     result3 = k / result2
+#     finish_result = V_engine1 / result3
+#     print(finish_result)
+
+
+print("---------------------------------------------------------------------------------------------------------------")
+# print("#1")
+# a = int(input())
+# b = int(input())
+# if a > b:
+#     print(b)
+# else:
+#     print(a)
+
+# print("#2")
+# a = int(input())
+# if a > 0:
+#     print("1")
+# elif a == 0:
+#     print("0")
+# else:
+#     print("-1")
+
+
+# print("#3")
+# a = int(input())
+# if a < 0:
+#      print("Choose another number, please.")
+# elif int(a / 4) == float(a / 4) and int(a / 400) == float(a / 400):
+#     print("OK")
+# elif int(a / 100) != float(a / 100):
+#     print("NOT OK")
+# else:
+#     print("NOT OK")
+#     print("НЕПРАВИЛЬНО")
+#
+# year = 300
+# if year % 4 == 0 and year % 100 != 0 or year % 400 == 0
+#     print("OK")
+# else:
+#     print("NOT OK")
+
+
+
+
+
+# print("#4")
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# print ("можно так") a,b,c = 3,4,5
+# if a <= b and a <= c:
+#     print(a)
+# elif b <= a and b <= c:
+#     print(b)
+# else:
+#     print(c)
+
+
+# print("#5")
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# if a == b and b == c:
+#     print("3")
+# elif a == b:
+#     print("2")
+# elif a == c:
+#     print("2")
+# elif b == c:
+#     print("2")
+# else:
+#     print("0")
+
+# if not 1 > 2:
+#     print("not worked!")
+# else:
+#     print("not worked")
+
+
+# print("#6")
+#     a,b,c = 4,2,5
+#
+#     if a > b:
+#     a,b = b,a
+#     if b > c:
+#     b,c = c,b
+#     if a > b:
+#     a,b = b,a
+#     print(a,b,c)
+
+# a = 3
+# b = 5
+# print("ДО",a,b)
+# a,b = b,a
+# print("ПОСЛЕ",a,b)
+
+
+
+
+
+
