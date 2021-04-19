be7409a54a8712bd8c57f5020ce80a95a990ab50
aliyaandabekova <andabekovaaliyaa@gmail.com>
2021-04-19 16:13:21 +0300

    create 08.04(if_else_elif)

---
+++ 08.04(print_int_float_str).py
@@ -0,0 +1,43 @@
+#  print(2+2)
+#  print(2*2)
+# print(type(5 > 2))
+# print(5 % 2 == 0)
+# print(5 < 2)
+# print(5 != 2)
+# name= "aliya"
+# print('Hello, ',name)
+# number1 = int(input())
+# number2 = int(input())
+# summary = number1 + number2
+#
+# print(summary / 2)
+# a = 5
+# b = a
+# a = 15
+# print(b)
+
+# price = "500"
+# price = int(price)
+# print(isinstance(price,int))
+# price = "50.0"
+# print(isinstance(price,str))
+
+# x = int(input())
+# print(x % 10)
+
+# x = int(input())
+# print(x // 100)
+
+# a,b,c,d = 4, 5, 6, 7
+# print("a =",a, "b =",b, "c =",c, "d =",d )
+
+
+i = 58565949.2456585
+isinstance(i,float)
+
+
+
+
+
+
+
