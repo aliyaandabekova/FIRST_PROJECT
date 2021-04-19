747b69879d460584a6b8347c4e0fef2308561fd5
aliyaandabekova <andabekovaaliyaa@gmail.com>
2021-04-19 17:33:20 +0300

    while_list

---
+++ 13.04(while_list).py
@@ -0,0 +1,185 @@
+# count = 0
+# while  count < 100:
+#     count = count + 1
+#     print(count)
+
+# count = 0
+# while  count < 100:
+#     count += 1
+#     print(count)
+
+# count = 0
+# sum = 0
+# while  count < 100:
+#     count += 1
+#     sum += count
+#     print(count,sum)
+
+# str1 = "milk"
+# print(str1[1])
+
+# str1 = "milk"
+# i = 0
+# while i < len(str1):
+#     print(str1[i])
+#     i = i + 1
+
+# print("ТЕМА: ЦИКЛ")
+
+# i = 0
+# while i < 10:
+#     print(i)
+#     i = i + 1
+
+# i = 0
+# sum = 0
+# while i < 10:
+#     i = i + 1
+#     sum = sum + i
+# print(sum)
+
+# i = 0
+# sum = 0
+# while i < 10:
+#     i = i + 1
+#     sum = sum + i
+#     if sum == 45:
+#         break
+# print(sum)
+import random
+
+# i = 0
+# sum = 0
+# secret = random.randint(1,3)
+# print(secret)
+# while i < secret:
+#     i = i + 1
+#     sum = sum + i
+# print(secret)
+# print(sum)
+
+# import random
+#
+# i = 0
+# secret = 0
+# j = 0
+# while i < 10:
+#     secret = random.randint(1,1000)
+#     print("СЛУЧАЙНОЕ ЧИСЛО ",secret,"под номером: ",i)
+#     if secret > j:
+#         j = secret
+#     i += 1
+# print("Наибольшее число: ",j)
+
+# import random
+# i = 0
+# secret = 0
+# j = 1000
+# while i < 10:
+#     secret = random.randint(1,1000)
+#     print("СЛУЧАЙНОЕ ЧИСЛО ",secret,"под номером: ",i)
+#     if secret < j:
+#         j = secret
+#     i += 1
+# print("Наименьшее число: ",j)
+
+# years = 2
+# money = int(input())
+# percentage = 10
+# i = 0
+# while i < years:
+#     i += 1
+#     money = money + (money * percentage / 100)
+# print(money)
+
+# years = 2
+# money = int(input())
+# percentage = 10
+# i = 0
+# while i < years and money > 0:
+#     i += 1
+#     money = money + (money * percentage / 100)
+# print(money)
+
+# years = 2
+# money = int(input())
+# percentage = 10
+# i = 0
+# if money > 0:
+#     while i < years:
+#         i += 1
+#         money = money + (money * percentage / 100)
+#     print(money)
+# else:
+#     print("Недопустимая сумма!")
+
+# i = 0
+# while i < 1000000:
+#     i += 1
+#     print(i)
+
+# i = 0
+# while i < 1000:
+#     print(i)
+#     i += 1
+
+# print("ТЕМА: СПИСКИ")
+
+# list1 = [1, 2, 3, 4, 5]
+# print(len(list1))
+
+# list2 = ["Vova", 2004, "M", "18",["harry", "das"]]
+# print(list2)
+
+# list2 = ["Vova", 2004, "M", "18",["harry", "das"]]
+# print(list2[4][0])
+
+# list1 = ["Vova", 2004, "M", "18"]
+# i = 0
+# while i < len(list1):
+#     print(list1[i])
+#     i += 1
+
+# list1 = ["Begimay", "Jony", "Frank", "Fred", "Obama", "Trump", "Boris", "Steven", "Cat"]
+# criminal = input("Enter criminal's name: ")
+# if criminal in list1
+#     print("Преступник найден!")
+# else:
+#     print("Город чист!")
+
+# list1 = ["Begimay", "Jony", "Frank", "Fred", "Obama", "Trump", "Boris", "Steven", "Cat"]
+# criminal = input("Enter criminal's name: ")
+# i = 0
+# while i < len(list1):
+#     if criminal == list1[i]:
+#         print("Адрес криминала: ",i)
+#         break
+#     else:
+#         print(("Не найден!"))
+#     i += 1
+
+# list1 = ["Begimay", "Jony", "Frank", "Fred", "Obama", "Trump", "Boris", "Steven", "Cat"]
+# criminal = input("Enter criminal's name: ")
+# i = 0
+# while i < len(list1):
+#     if criminal == list1[i]:
+#         print("Адрес криминала: ",i)
+#     else:
+#         print(("Не найден!"))
+#     i += 1
+
+# list1 = ["Begimay", "Jony", "Frank", "Fred", "Obama", "Trump", "Boris", "Steven", "Cat"]
+# criminal = input("Enter criminal's name: ")
+# i = 0
+# while i < len(list1):
+#     if criminal in list1:
+#         if criminal == list1[i]:
+#             print("Адрес криминала: ",i)
+#     else:
+#         print(("Не найден!"))
+#         break
+#     i += 1
+
+# print("ДОМАШНЕЕ ЗАДАНИЕ")
+
+
