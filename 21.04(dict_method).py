cb875d59d1c03ad2376df542529154a97c81887d
aliyaandabekova <andabekovaaliyaa@gmail.com>
2021-04-22 17:49:50 +0300

    методы словарей

---
+++ 21.04(dict_method).py
@@ -0,0 +1,63 @@
+# dict1 = {"name":"Honda" }
+
+# print(dict1["name"])
+#
+# print(dict1.get("name"))
+#
+# dict1["model"] = "fit"
+# print(dict1)
+
+# print(dict1.keys())
+
+# print(dict1.values())
+
+# print(dict1.items())
+
+# print(dict1.pop("name"))
+# print(dict1)
+
+
+
+
+# dict2 = {"names":['honda','bmw','mercedes']}
+
+# dict2['names'].append('bugatti')
+# print(dict2)
+
+
+
+# string1 = 'jfgkoKjk jfiJrko nklfoklpr jfikfoKldkl nijfkLKorkf nfjodHl opg nbit migk opg'
+
+# string1 = string1.capitalize()
+# print(string1)
+
+
+
+
+
+# apples = 10
+# print(f'За день я съела {apples} и {20} груш')
+
+
+
+# list1 = ['red', 'blue', 'white']
+# dict1 = {'name':'aliya'}
+
+# for i,color in enumerate(list1):
+#     print(i,color)
+
+
+# for key,value in dict1.items():
+#     print(key,value)
+
+# iterat = zip(list1)
+# print(iterat)
+
+# list1 = ['red', 'blue', 'white']
+# file1 = open('homework.py','r')
+# file_data = file1.read()
+# print(file_data)
+
+
+
+
